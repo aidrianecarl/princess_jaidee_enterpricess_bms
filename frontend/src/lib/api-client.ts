f1a54f9528d20e0832c615aefc56dee8aafbbf43
Aidriane Carl Esmena <aidrianecarlesmena@gmail.com>
2026-02-24 16:23:45 +0800

    Update api-client.ts

--- frontend/src/lib/api-client.ts
+++ frontend/src/lib/api-client.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance, AxiosError } from 'axios'
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.princessjaideeenterprises.com/api'
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.princessjaideeenterprises.com/api/api'
 
 interface ApiError {
   message: string
