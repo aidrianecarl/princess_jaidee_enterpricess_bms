f08050abf539af948d5f7f1c4c8ca69f4a5e07b3
Aidriane Carl Esmena <aidrianecarlesmena@gmail.com>
2026-03-28 11:49:39 +0800

    Update sidebar.tsx

--- frontend/src/components/dashboard/sidebar.tsx
+++ frontend/src/components/dashboard/sidebar.tsx
@@ -25,9 +25,14 @@ export function DashboardSidebar() {
       </div>
 
       {/* Sidebar */}
-      <aside className={`fixed md:fixed top-14 sm:top-16 left-0 w-64 bg-white border-r border-gray-200 h-[calc(100vh-56px)] sm:h-[calc(100vh-64px)] transition-all duration-300 z-30 ${
-        mobileMenuOpen ? "left-0" : "-left-64"
-      }`}>
+      <aside
+        className={`fixed top-14 sm:top-16 left-0 w-64 
+        bg-white border-r border-gray-200 
+        h-[calc(100vh-56px)] sm:h-[calc(100vh-64px)] 
+        overflow-y-auto
+        transition-all duration-300 z-30 
+        ${mobileMenuOpen ? "left-0" : "-left-64 md:left-0"}`}
+        >
       <nav className="p-6 space-y-2">
         {/* Main Menu */}
         <Link
