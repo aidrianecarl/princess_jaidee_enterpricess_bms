823784cac11d9ae4c3a8ad26c2618f137ae3e575
Aidriane Carl Esmena <aidrianecarlesmena@gmail.com>
2026-03-28 11:05:32 +0800

    Update sidebar.tsx

--- frontend/src/components/dashboard/sidebar.tsx
+++ frontend/src/components/dashboard/sidebar.tsx
@@ -101,8 +101,6 @@ export function DashboardSidebar() {
                 </div>
               </div>
             </div>
-              )}
-            </div>
           )}
         </div>
       </nav>
