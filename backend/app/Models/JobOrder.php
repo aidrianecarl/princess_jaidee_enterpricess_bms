0d17d9bd0088a546170b86e3de0539ad3de1f5fd
Aidriane Carl Esmena <aidrianecarlesmena@gmail.com>
2026-04-04 15:31:33 +0800

    Update JobOrder.php

--- backend/app/Models/JobOrder.php
+++ backend/app/Models/JobOrder.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class JobOrder extends Model
 {
@@ -45,10 +44,5 @@ class JobOrder extends Model
     public function assignedTo(): BelongsTo
     {
         return $this->belongsTo(User::class, 'assigned_to');
-    }
-
-    public function items(): HasMany
-    {
-        return $this->hasMany(JobOrderItem::class);
     }
 }
