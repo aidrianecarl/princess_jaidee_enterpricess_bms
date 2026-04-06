e7a0fb41f1b0576c051764745e131a8159a4f915
Aidriane Carl Esmena <aidrianecarlesmena@gmail.com>
2026-04-07 07:38:22 +0800

    new update

--- frontend/src/app/admin/job-orders/[jobOrderId]/orders/page.tsx
+++ frontend/src/app/admin/job-orders/[jobOrderId]/orders/page.tsx
@@ -586,7 +586,6 @@ export default function JobOrderDetailPage() {
                               </div>
                             )}
                           </div>
-                        )}
                       </div>
                     )
                   })}
